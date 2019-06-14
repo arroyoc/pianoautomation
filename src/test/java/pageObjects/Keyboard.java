e699de1e7d2055e25b0d7cc490e632d1484b64f4
Chris Arroyo <arroyoc91@gmail.com>
2019-06-14 10:30:23 -0600

    added 2 notes

--- src/test/java/pageObjects/Keyboard.java
+++ src/test/java/pageObjects/Keyboard.java
@@ -1,10 +1,24 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Keyboard extends PageBase{
 
     public Keyboard(WebDriver wdriver){
         super(wdriver);
+    }
+
+    private By firstNote = By.id("a84d");
+    private By secondNote = By.id("a89d");
+
+    public Keyboard pressFirstNote(){
+        waitAndClick(firstNote);
+        return this;
+    }
+
+    public Keyboard pressSecondNote(){
+        waitAndClick(secondNote);
+        return this;
     }
 }
