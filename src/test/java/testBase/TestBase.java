b92ee081ef5ab8c1a3f6d08293a2304abbb26911
Chris Arroyo <arroyoc91@gmail.com>
2019-06-14 10:12:58 -0600

    updated pom

---
+++ src/test/java/testBase/TestBase.java
@@ -0,0 +1,21 @@
+package testBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+
+public class TestBase {
+
+    protected static WebDriver wdriver;
+
+    protected static void webSetup() throws IOException {
+
+        // Check if web driver has already been instantiated
+        // Driver options are 'PhantomJSDriver', 'ChromeDriver', 'FireFoxDriver'
+        if (wdriver == null) {
+            System.setProperty("webdriver.chrome.driver", "./../QA-SmallStore//src//test//java//webDrivers//ChromeDriver//chromedriver.exe");
+            wdriver = new ChromeDriver();
+        }
+    }
+}
